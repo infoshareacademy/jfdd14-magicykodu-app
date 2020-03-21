8bee863d51c2c216c6821895794f57dc57b75067
Karolina <karolina.szlenk@gmail.com>
2020-03-21 13:58:57 +0100

    New links added

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
 
 class Sidebar extends Component{
     render(){
@@ -8,6 +8,10 @@ class Sidebar extends Component{
                 <Link to="/">Home</Link>
                 <br></br>
                 <Link to="/lista-wydarzen">Lista wydarzeń</Link>
+                <br></br>
+                <Link to="/stworz-wlasne-wydarzenie">Stwórz własne wydarzenie</Link>
+                <br></br>
+                <Link to="/ulubione">Ulubione</Link>
             </nav>
         )
     }
