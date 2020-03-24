1c4e1a2a923af322f089325d40b75c30066cc15c
Karolina <karolina.szlenk@gmail.com>
2020-03-24 19:17:01 +0100

    Styles mote to css file

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,18 +4,6 @@ import AreaChartUsers from '../../components/AreaChartEvents/AreaChartEvents'
 import PieChartAge from '../../components/PieChartAge/PieChartAge'
 import './Home.css'
 
-const headerStyle = {
-    textAlign: "center"
-}
-
-const itemStyle = {
-    boxShadow: "1px 1px 5px 1px #999999",
-    backgroundColor: "#fff",
-    borderRadius: "5px",
-    padding: "20px",
-    margin: "15px",
-}
-
 const data = [
     {id: 1, icon: 'user', header: 12184, description: 'Wszystkich użytkowników', color: '#8884d8'},
     {id: 2, icon: 'male', header: "63 lata", description: 'Wiek najstarszego użytkownika', color: '#999999'},
@@ -30,7 +18,7 @@ const data = [
 export default class Home extends Component {
     render() {
         const appData = data.map(item => 
-            <Grid.Column key={item.id} mobile={12} tablet={6} computer={3} style={itemStyle}>
+            <Grid.Column key={item.id} mobile={14} tablet={7} computer={3}>
                 <Item verticalAlign='middle'>
                     <Icon style={{color:item.color}} name={item.icon} size='huge'/>
                     <Item.Content>
@@ -44,17 +32,17 @@ export default class Home extends Component {
                 </Item>          
             </Grid.Column>)
         return (
-            <Grid padded='vertically' textAlign='center'> 
+            <Grid textAlign='center'> 
                 <Grid.Row>
                     {appData}
                 </Grid.Row>
                 <Grid.Row>
-                    <Grid.Column mobile={13} tablet={13} computer={6}>
-                        <h3 style={headerStyle}>Przeprowadzone imprezy 2020</h3>
+                    <Grid.Column mobile={14} tablet={14} computer={6}>
+                        <h3 className="home__heading">Przeprowadzone imprezy 2020</h3>
                         <AreaChartUsers />
                     </Grid.Column>
-                    <Grid.Column mobile={13} tablet={13} computer={6}>
-                        <h3 style={headerStyle}>Wiek użytkowników</h3>
+                    <Grid.Column mobile={14} tablet={14} computer={6}>
+                        <h3 className="home__heading">Wiek użytkowników</h3>
                         <PieChartAge />
                     </Grid.Column>
                 </Grid.Row>  
