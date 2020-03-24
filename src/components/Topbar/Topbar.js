dc1dd32aa723fec99e3eb93d05bb8b64090a4541
Karolina <karolina.szlenk@gmail.com>
2020-03-24 11:39:58 +0100

    Hamburger menu created

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -1,33 +1,50 @@
 import React, {Component} from 'react'
-// import './Topbar.css';
 import { Menu, Segment } from 'semantic-ui-react'
 import Logo from '../Logo/Logo'
-
+import MySidebar from './../MySidebar/MySidebar'
+import './Topbar.css'
 
 export default class Topbar extends Component {
     state = { 
-      activeItem: 'home' 
+      menuVisible: false
+    }
+
+    changeMenuVisibility = () => {
+      this.setState({
+        menuVisible: !this.state.menuVisible
+      })
     }
   
     handleItemClick = (e, { name }) => this.setState({ activeItem: name })
   
     render() {
-      const { activeItem } = this.state
-  
+      const { menuVisible } = this.state
+
       return (
-        <Segment inverted style={{margin: '0px'}}>
-          <Menu inverted secondary>
-            <Logo />
-            <Menu.Item
-              position='right'
-              name='Log out'
-              active={activeItem === 'home'}
-              onClick={this.handleItemClick}
-            />
-          </Menu>
-        </Segment>
+        <div >
+          <Segment inverted>
+            <Menu inverted secondary>
+              <Menu.Item 
+                  
+                  icon="bars"
+                  active
+                  onClick={this.changeMenuVisibility} />
+              <Menu.Item className='logo__item'>
+                <Logo 
+                position='left'/>
+              </Menu.Item  >
+              
+              <Menu.Item
+                position='right'
+                name='Log out'
+                active
+                onClick={this.handleItemClick} />
+            </Menu>
+          </Segment>
+          <MySidebar visibility={menuVisible} />
+        </div>  
       )
     }
-  }
+}
   
 
