ecd4dcf3d41e0a45b3f6665f27c76ab1a9419bfa
Karolina <karolina.szlenk@gmail.com>
2020-03-24 16:03:31 +0100

    Links changed

--- src/components/MySidebar/MySidebar.js
+++ src/components/MySidebar/MySidebar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { Menu, Icon, Sidebar} from 'semantic-ui-react'
 import './MySidebar.css'
 
-class MySidebar extends Component{
+export default class MySidebar extends Component{
     
     render(){
         const visibility = this.props.visibility
@@ -14,8 +14,8 @@ class MySidebar extends Component{
             fontWeight: "400",
             backgroundColor: "#28bb76",
             marginTop: "40px"
-
         };
+        
         return (
             <Sidebar style={sidebarStyle} 
             as={Menu}
@@ -25,34 +25,25 @@ class MySidebar extends Component{
             vertical
             visible={visibility ? false : true}
             width='thin'>
-                <Menu.Item as='a'> 
-                    <Link to="/" style={{fontWeight: "400", color: "#1e1e1e"}}>
-                        <Icon name='home' />
-                        Home
-                    </Link>
+                <Menu.Item as={ Link } to='/' style={{fontWeight: "400", color: "#1e1e1e"}}> 
+                    <Icon name='home' />
+                    Home
                 </Menu.Item>
-                <Menu.Item as='a'>  
-                    <Link to="/lista-wydarzen" style={{fontWeight: "400", color: "#1e1e1e"}}>
-                        <Icon name='calendar alternate outline' />
-                        Lista wydarzeń
-                    </Link>
+                <Menu.Item as={ Link } to="/lista-wydarzen" style={{fontWeight: "400", color: "#1e1e1e"}}>  
+                    <Icon name='calendar alternate outline' />
+                    Lista wydarzeń   
                 </Menu.Item>
-                <Menu.Item as='a'> 
-                    <Link to="/stworz-wlasne-wydarzenie" style={{fontWeight: "400", color: "#1e1e1e"}}>
-                        <Icon name='calendar plus outline' /> 
-                        Stwórz własne wydarzenie
-                    </Link>
+                <Menu.Item as={ Link } to="/stworz-wlasne-wydarzenie" style={{fontWeight: "400", color: "#1e1e1e"}}> 
+                    <Icon name='calendar plus outline' /> 
+                    Stwórz własne wydarzenie
                 </Menu.Item>
-                <Menu.Item as='a'>      
-                    <Link to="/ulubione" style={{fontWeight: "400", color: "#1e1e1e"}}>
-                        <Icon name='heart' />
-                        Ulubione
-                    </Link>
+                <Menu.Item as={ Link } to="/ulubione" style={{fontWeight: "400", color: "#1e1e1e"}}>      
+                    <Icon name='heart' />
+                    Ulubione
                 </Menu.Item>
-        </Sidebar>      
+            </Sidebar>      
         )
     }
 }
 
-export default MySidebar;
 
