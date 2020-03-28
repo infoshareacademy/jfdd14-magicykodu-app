047be297f62bdc95e079fa36a8f374bae14fe0f4
Karolina <karolina.szlenk@gmail.com>
2020-03-28 15:30:32 +0100

    Form layout created

--- src/components/AddEventForm/AddEventForm.js
+++ src/components/AddEventForm/AddEventForm.js
@@ -1,47 +1,98 @@
 import React, { Component } from 'react'
 import { Button, Form, Header } from 'semantic-ui-react'
-import SemanticDatepicker from 'react-semantic-ui-datepickers';
+import DatePicker from 'react-datepicker';
 
+import "react-datepicker/dist/react-datepicker.css";
 import './AddEventForm.css'
 
-const options = [
-    { key: 'u', text: 'Uliczny', value: 'uliczny' },
-    { key: 'p', text: 'Przełajowy', value: 'przelajowy' },
+const typeOfRun = [
+    { key: 'u', text: 'Uliczny', value: 'street' },
+    { key: 'p', text: 'Przełajowy', value: 'cross-country' },
+]
+
+const typeOfDistance = [
+    { key: '5', text: '5 km', value: '5' },
+    { key: '10', text: '10 km', value: '10' },
+    { key: 'half-m', text: 'Półmaraton', value: 'half-marathon' },
+    { key: 'm', text: 'Maraton', value: 'marathon' },
+    { key: 'o', text: 'Inne', value: 'Other' },
 ]
 
 export default class AddEventForm extends Component {
-    state = {}
 
-    handleChange = (e, { value }) => this.setState({ value })
+    state = {
+        date: ''
+    }
+
+    handleClick = () => {
+
+    }
+
+    handleDate = (date) => {
+        this.setState({ date: date })   
+    }
+
+    onFormSubmit = (e) => {
+        e.preventDefault();
+    }
 
     render() {
-        const { value } = this.state
         return(
             <div className='form__container'>
                 <Header>Stwórz własne wydarzenie</Header>
                 <hr></hr>
-                <br></br>
-                
-                <Form>
-                    <Form.Input required label='Nazwa' placeholder='Nazwa biegu' />
-                    <Form.Input required label='Lokalizacja' placeholder='Lokalizacja' />
-                    <Form.Field required>
-                        <label>Data</label>
-                        <SemanticDatepicker/>
+                <br></br>     
+                <Form onSubmit={ this.onFormSubmit }>
+                    <Form.Input 
+                        label='Nazwa' 
+                        placeholder='Nazwa' 
+                        required />
+                    <Form.Input 
+                        label='Lokalizacja' 
+                        placeholder='Lokalizacja' 
+                        required/>
+                    <Form.Field required >
+                        <label>Data i godzina</label>
+                        <DatePicker
+                            selected={ this.state.date }
+                            onChange={ this.handleDate}
+                            showTimeSelect
+                            name='date'
+                            timeIntervals={30}
+                            timeCaption='time'
+                            placeholderText='MM/DD/RRRR g:mm'
+                            dateFormat='MMMM d, yyyy h:mm'/>
                     </Form.Field>
-                    
                     <Form.Select
                         fluid
-                        required
+                        label='Dystans'
+                        options={typeOfDistance}
+                        placeholder='Dystans'
+                        required/> 
+                    <Form.Select
+                        fluid
                         label='Rodzaj biegu'
-                        options={options}
+                        options={typeOfRun}
                         placeholder='Rodzaj biegu'
-                    /> 
-                    <Form.Input required label='Liczba uczestników' placeholder='Liczba uczestników' />
-                    <Form.Input label='Opłata' placeholder='Opłata' />
-                    
-                    <Form.Checkbox label='Zgadzam się z warunkami tworzenia własnych wydarzeń' />
-                    <Button type='submit' color='grey'>Submit</Button>
+                        required/> 
+                    <Form.Input 
+                        label='Liczba uczestników' 
+                        placeholder='Liczba uczestników'  
+                        required/>
+                    <Form.Input 
+                        label='Opłata' 
+                        placeholder='Opłata'/>
+                    <Form.Input 
+                        label='Zdjęcie wydarzenia' 
+                        placeholder='Adres URL' />
+                    <br></br>
+                    <Button 
+                        type='submit' 
+                        color='grey'
+                        // onClick={}
+                        >
+                        Dodaj wydarzenie
+                    </Button>
                 </Form>
             </div>         
         )
