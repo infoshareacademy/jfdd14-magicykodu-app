1b5ce14258da9e855b545d366f02c50e2bcc73de
Karolina <karolina.szlenk@gmail.com>
2020-03-29 10:17:44 +0200

    State added

--- src/components/AddEventForm/AddEventForm.js
+++ src/components/AddEventForm/AddEventForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Form, Header } from 'semantic-ui-react'
+import { Button, Form, Header, Modal } from 'semantic-ui-react'
 import DatePicker from 'react-datepicker';
 
 import "react-datepicker/dist/react-datepicker.css";
@@ -21,16 +21,29 @@ const typeOfDistance = [
 export default class AddEventForm extends Component {
 
     state = {
-        date: ''
+        name: '',
+        place: '',
+        date: '',
+        distance: '',
+        run: '',
+        numOfrunners: '',
+        charge: '',
+        img: ''
     }
 
-    handleClick = () => {
-
+    handleClick = (props) => {
+        this.props.handleClick()
     }
 
     handleDate = (date) => {
         this.setState({ date: date })   
     }
+
+    handleChange = (e, { name, value }) => {
+        if (this.state.hasOwnProperty(name)) {
+          this.setState({ [name]: value });
+        }
+    };
 
     onFormSubmit = (e) => {
         e.preventDefault();
@@ -44,20 +57,26 @@ export default class AddEventForm extends Component {
                 <br></br>     
                 <Form onSubmit={ this.onFormSubmit }>
                     <Form.Input 
+                        name="name"
+                        value={this.state.name}
+                        onChange={this.handleChange}
                         label='Nazwa' 
                         placeholder='Nazwa' 
                         required />
                     <Form.Input 
+                        name="place"
+                        value={this.state.place}
+                        onChange={this.handleChange}
                         label='Lokalizacja' 
                         placeholder='Lokalizacja' 
                         required/>
                     <Form.Field required >
                         <label>Data i godzina</label>
                         <DatePicker
-                            selected={ this.state.date }
-                            onChange={ this.handleDate}
-                            showTimeSelect
                             name='date'
+                            selected={this.state.date}
+                            onChange={this.handleDate}
+                            showTimeSelect
                             timeIntervals={30}
                             timeCaption='time'
                             placeholderText='MM/DD/RRRR g:mm'
@@ -65,34 +84,54 @@ export default class AddEventForm extends Component {
                     </Form.Field>
                     <Form.Select
                         fluid
+                        name='distance'
+                        value={this.state.distance}
+                        onChange={this.handleChange}
                         label='Dystans'
                         options={typeOfDistance}
                         placeholder='Dystans'
                         required/> 
                     <Form.Select
                         fluid
+                        name='run'
+                        value={this.state.run}
+                        onChange={this.handleChange}
                         label='Rodzaj biegu'
                         options={typeOfRun}
                         placeholder='Rodzaj biegu'
                         required/> 
                     <Form.Input 
+                        name='numOfrunners'
+                        value={this.state.numOfrunners}
+                        onChange={this.handleChange}
                         label='Liczba uczestników' 
+                        type='number'
                         placeholder='Liczba uczestników'  
                         required/>
                     <Form.Input 
+                        name='charge'
+                        value={this.state.charge}
+                        onChange={this.handleChange}
                         label='Opłata' 
+                        type='number'
                         placeholder='Opłata'/>
                     <Form.Input 
+                        name='img'
+                        value={this.state.img}
+                        onChange={this.handleChange}
                         label='Zdjęcie wydarzenia' 
                         placeholder='Adres URL' />
-                    <br></br>
-                    <Button 
-                        type='submit' 
-                        color='grey'
-                        // onClick={}
-                        >
-                        Dodaj wydarzenie
-                    </Button>
+                    <br></br>                
+                    <Modal   
+                        trigger={<Button 
+                            type='submit' 
+                            color='grey'
+                            onClick={this.handleClick}>
+                            Dodaj wydarzenie
+                        </Button>} 
+                        header='Gratulacje!'
+                        content='Stworzyłeś własne wydarzenie.'
+                        actions={[{ content: 'Ok', positive: true }]}/>             
                 </Form>
             </div>         
         )
--- src/pages/AddEvent/AddEvent.js
+++ src/pages/AddEvent/AddEvent.js
@@ -2,10 +2,17 @@ import React, {Component} from 'react'
 import AddEventForm from '../../components/AddEventForm/AddEventForm'
 
 class AddEvent extends Component {
+    state = { showModal: false }
+
+    onClickHandle = () => {
+        this.setState({ showModal: true })
+    }
+
     render() {
+        console.log(this.state.showModal)
         return (
             <div>
-               <AddEventForm />
+               <AddEventForm showModal={this.state.showModal} handleClick={this.onClickHandle}/>
             </div>
         )
     }
