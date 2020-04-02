f492607700b212670abe1d26ead537a4cea362f4
Karolina <karolina.szlenk@gmail.com>
2020-04-02 12:56:24 +0200

    Dropdown updated

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,33 +1,33 @@
-import React, { Component } from 'react'
-import { Grid, Input, Dropdown } from 'semantic-ui-react';
-import List from '../../List.json'
+import React, { Component } from "react";
+import { Grid, Input, Dropdown } from "semantic-ui-react";
+// import List from '../../List.json'
 
-export default class Search extends Component{
-    render(){
-        return(
-            <div>
-                <Grid padded={true}>
-                    <Grid.Row columns={1} centered={true}>
-                        <Grid.Column mobile={14} tablet={14} computer={4}>
-                            <Input
-                                placeholder='Wpisz miasto'
-                                fluid
-                            />
-                        </Grid.Column>
-                        <Grid.Column mobile={14} tablet={14} computer={4}>
-                            <Dropdown
-                                clearable
-                                fluid
-                                selection 
-                                placeholder='Wybierz rodzaj biegu'
-                            />
-                        </Grid.Column>
-                        <Grid.Column mobile={14} tablet={14} computer={4}>
-                           
-                        </Grid.Column>
-                    </Grid.Row> 
-                </Grid>
-            </div>
-        )
-    }
+const typeOfRun = [
+  { key: "u", text: "Uliczny", value: "street" },
+  { key: "p", text: "Przełajowy", value: "cross-country" }
+];
+
+export default class Search extends Component {
+  render() {
+    return (
+      <div>
+        <Grid padded={true}>
+          <Grid.Row columns={1} centered={true}>
+            <Grid.Column mobile={14} tablet={14} computer={4}>
+              <Input placeholder="Wpisz miasto" fluid />
+            </Grid.Column>
+            <Grid.Column mobile={14} tablet={14} computer={4}>
+              <Dropdown
+                fluid
+                selection
+                options={typeOfRun}
+                placeholder="Wybierz rodzaj biegu"
+              />
+            </Grid.Column>
+            <Grid.Column mobile={14} tablet={14} computer={4}></Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
+    );
+  }
 }
