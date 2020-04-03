691c14c9eaebe99726e587078c09c726cf8b56a7
Karolina <karolina.szlenk@gmail.com>
2020-04-03 19:12:54 +0200

    file corrected

--- src/index.js
+++ src/index.js
@@ -1,31 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.min.css';
-import App from './App';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import App from './App'
 
 import './index.css';
-
-import Topbar from './components/Topbar/Topbar';
-import Sidebar from './components/MySidebar/MySidebar'
-import Home from './pages/Home/Home'
-import EventList from './pages/EventList/EventList'
-import Default from './pages/Default/Default'
-
-
-
-ReactDOM.render(
-    <BrowserRouter>
-        <Topbar/>
-        <Sidebar/>
-        
-        <Switch>      
-            <Route exact path='/' component={Home} />
-            <Route path='/lista-wydarzen' component={EventList} />
-            <Route component={Default} />
-        </Switch>  
-    </BrowserRouter>, 
-    document.getElementById('root')
-);
+import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render( <App/>, document.getElementById('root'));
